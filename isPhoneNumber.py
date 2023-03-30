12f8dc6e1ef690e3efaeddbb90b2a429c4b03552
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-30 16:55:19 -0700

    Add brut force method to determine phone number validity

---
+++ isPhoneNumber.py
@@ -0,0 +1,35 @@
+
+# A non-regex approach to determining if a sequence of characters
+# is a phone number
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+
+    if text[3] != '-':
+        return False
+    
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+
+    if text[7] != '-':
+        return False
+
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    
+    return True
+
+
+
+print('Is 415-555-4242 a phone number?')
+print(isPhoneNumber('415-555-4242'))
+
+print('Is Moshi moshi a phone Number?')
+print(isPhoneNumber('Moshi moshi'))

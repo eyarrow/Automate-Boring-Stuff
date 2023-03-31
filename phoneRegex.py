2078bb281ea312283eb996cc6c6fa7c075f30666
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-30 17:39:03 -0700

    Practice with wildcards

--- phoneRegex.py
+++ phoneRegex.py
@@ -20,3 +20,34 @@ areaCode, mainNumber = mo.groups()
 print(areaCode)
 print(mainNumber)
 
+phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My phone number is (503) 290-7777.')
+print(mo.group(1))
+print(mo.group(2))
+
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+print(mo1.group())
+
+mo2 = heroRegex.search('Tina Fey and Batman')
+print(mo2.group())
+
+# Search for Batman, Batmobile, Batcopter, Batbat
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost its wheel')
+print(mo.group())
+
+# The question mark deals with optional Characters
+batRegex = re.compile(r'Bat(wo)?man')
+mo = batRegex.search('The adventures of Batman')
+print(mo.group())
+
+# We can use this to help the program identify phone numbers with the area code
+# as optional
+
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+mo = phoneRegex.search('my phone number is 290-6666')
+mo2 = phoneRegex.search('my phone number is 503-290-6666')
+print(mo.group())
+print(mo2.group())
+

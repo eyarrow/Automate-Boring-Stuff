5fc1fa8d580b3a8eef2e133463663b42fa52ffb0
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-30 17:15:42 -0700

    Add regex grouping

--- phoneRegex.py
+++ phoneRegex.py
@@ -6,3 +6,17 @@ phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
 mo = phoneNumRegex.search('My number is 415-555-4242')
 print('Phone number found: ' + mo.group())
 
+# Use parantheses to produce groupings
+phoneNumRex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRex.search('My number is 503-290-6666')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
+print(mo.groups())
+
+areaCode, mainNumber = mo.groups()
+
+print(areaCode)
+print(mainNumber)
+
